22e0dc2c84aa78d0a7a6df5d271b868d51d55564
rakibtweets <rakibhasanroky0@gmail.com>
2021-09-22 03:41:54 +0600

    initial setup complete

---
+++ src/components/Header/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <div>
+      <header className="header">
+        <nav className="nav_items">
+          <img className="brand__logo" src="/logo192.png" alt="" />
+        </nav>
+      </header>
+    </div>
+  );
+};
+
+export default Header;

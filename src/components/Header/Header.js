06cceb9960d04b759eae0641f017a984b9705c40
rakibtweets <rakibhasanroky0@gmail.com>
2021-09-22 04:05:04 +0600

    header component complete

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,19 @@
 import React from 'react';
-
+import './Header.css';
 const Header = () => {
   return (
     <div>
       <header className="header">
         <nav className="nav_items">
-          <img className="brand__logo" src="/logo192.png" alt="" />
+          <div className="logo">
+            <img className="brand__logo" src="/logo192.png" alt="" />
+            <h2 className="logo__name">ReactJs</h2>
+          </div>
+          <div className="nav__list">
+            <a href="#0">Home</a>
+            <a href="#0">About</a>
+            <a href="#0">Work</a>
+          </div>
         </nav>
       </header>
     </div>
